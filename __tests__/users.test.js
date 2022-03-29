c4d9996ecf01bf403b5e423ee1466b3c0bd20f77
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-03-29 16:04:46 -0700

    users test to sign in existing user, service UserService function signIn, controller users route post /signin, model User functions getUserByEmail, authToken written and passing.

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secret routes', () => {
   beforeEach(() => {
@@ -21,6 +22,23 @@ describe('top-secret routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email: 'ryan@defense.gov',
+    });
+  });
+
+  it('should be able to sign in an existing user', async () => {
+    const user = await UserService.signUp({
+      email: 'ryan@defense.gov',
+      password: 'password-3',
+    });
+
+    const res = await request(app).post('/api/v1/users/signin').send({
+      email: 'ryan@defense.gov',
+      password: 'password-3',
+    });
+
+    expect(res.body).toEqual({
+      message: 'Successfully signed in!',
+      user,
     });
   });
 });
